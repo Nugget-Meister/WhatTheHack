6235a2291da51a34e96c687413d0f7bbd7d3dc02
Nugget-Meister <tremainecamacho@gmail.com>
2024-07-01 13:14:10 -0400

    switch markgrowgriddirty to dirtycache, unsure if works

--- Source/WhatTheHack/Buildings/Building_RogueAI.cs
+++ Source/WhatTheHack/Buildings/Building_RogueAI.cs
@@ -904,7 +904,8 @@ public class Building_RogueAI : Building
         }
 
         glowerComp.UpdateLit(Map);
-        Map.glowGrid.MarkGlowGridDirty(Position);
+        /*Map.glowGrid.MarkGlowGridDirty(Position);*/
+        Map.glowGrid.DirtyCache(Position);
     }
 
     private void GoRogue_HackMechs()
